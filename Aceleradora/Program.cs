a053432a93807ec8f800d73aa5ce77140e18b0d3
rafael eduardo rauber <rafael-rauber@hotmail.com>
2018-01-24 00:47:37 -0200

    Criação do projeto e do Branch inicial.

--- Aceleradora/Program.cs
+++ Aceleradora/Program.cs
@@ -6,10 +6,12 @@ using System.Threading.Tasks;
 
 namespace Aceleradora
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+
+
         }
     }
 }
