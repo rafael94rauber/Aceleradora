1b71f24b3d8f698f9e34fceab18753fa46c3f708
rafael eduardo rauber <rafael-rauber@hotmail.com>
2018-01-25 20:44:38 -0200

    Testes Automatizados e codigo liberado para a revisão

--- Aceleradora/Funcionario.cs
+++ Aceleradora/Funcionario.cs
@@ -9,14 +9,14 @@ namespace Aceleradora
         /// </summary>
         #region Constantes da classe
 
-        private const double TaxaJurosINSS = 0.10;
-        private const double TaxaSeguroVida = 0.15;
-        private const double TaxaJurosValeRefeicao = 0.11;
-        private const double TaxaJurosValeTransporte = 0.05;
+        public const double TaxaJurosINSS = 0.10;
+        public const double TaxaJurosSeguroVida = 0.15;
+        public const double TaxaJurosValeRefeicao = 0.11;
+        public const double TaxaJurosValeTransporte = 0.05;
 
         //utilizado para calcular o bonos de 20% do salario do funcionario a cada 1 ano de contrato com a empresa
-        private const int TotalDiasAno = 365;
-        private const double TaxaBonosAnual = 0.20;
+        public const int TotalDiasAno = 365;
+        public const double TaxaBonosAnual = 0.20;
         #endregion
 
         /// <summary>
@@ -36,21 +36,13 @@ namespace Aceleradora
             {
                 return CalcularJuros(TaxaJurosINSS);
             }
-            set
-            {
-                ValorInss = value;
-            }
         }
 
         public double ValorSeguroVida
         {
             get
             {
-                return CalcularJuros(TaxaSeguroVida);
-            }
-            set
-            {
-                ValorSeguroVida = value;
+                return CalcularJuros(TaxaJurosSeguroVida);
             }
         }
 
@@ -60,10 +52,6 @@ namespace Aceleradora
             {
                 return CalcularJuros(TaxaJurosValeRefeicao);
             }
-            set
-            {
-                ValorValeRefeicao = value;
-            }
         }
 
         public double ValorValeTransporte
@@ -72,21 +60,20 @@ namespace Aceleradora
             {
                 return CalcularJuros(TaxaJurosValeTransporte);
             }
-            set
-            {
-                ValorValeTransporte = value;
-            }
         }
 
         public double ValorBonosFuncionario
         {
             get
             {
-                return CalcularBonosAnual();
-            }
-            set
-            {
-                ValorBonosFuncionario = value;
+                TimeSpan tempoContratacao = DateTime.Now - Contratacao;
+
+                var bonosFuncionario = Salario * (int)tempoContratacao.TotalDays;
+
+                bonosFuncionario = bonosFuncionario / TotalDiasAno;
+                bonosFuncionario = bonosFuncionario * TaxaBonosAnual;
+
+                return bonosFuncionario;
             }
         }
 
@@ -105,18 +92,6 @@ namespace Aceleradora
         private double CalcularJuros(double taxaJuros)
         {
             return Salario * taxaJuros;
-        }
-
-        private double CalcularBonosAnual()
-        {
-            TimeSpan tempoContratacao = DateTime.Now - Contratacao ;
-
-            var bonosFuncionario = Salario * tempoContratacao.TotalDays;
-
-            bonosFuncionario = bonosFuncionario / TotalDiasAno;
-            bonosFuncionario = bonosFuncionario * TaxaBonosAnual;
-
-            return bonosFuncionario;
         }
     }
 }
