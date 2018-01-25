8f2ef058758a13f9ee0a50614a662f0048782b61
rafael eduardo rauber <rafael-rauber@hotmail.com>
2018-01-25 21:19:04 -0200

    Enviando explicação e alteração do nome da classe main

--- Aceleradora/ExecucaoSistema.cs
+++ Aceleradora/ExecucaoSistema.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace Aceleradora
 {
-    public class Program
+    public class ExecucaoSistema
     {
         private static Dictionary<int, Funcionario> ListaFuncionarios;
 
